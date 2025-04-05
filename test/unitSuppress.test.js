cb607eb05bd59e1f6a3dd50424e3865322370512
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-04-05 10:13:09 +0200

    ignore unsupported flags

--- test/unitSuppress.test.js
+++ test/unitSuppress.test.js
@@ -127,3 +127,47 @@ describe('Suppresses the tabular output', () => {
         )
     })
 })
+
+describe('Ignores unsupported flags', () => {
+    it('suppresses the tabular output, although there is an additional unsupported flag', () => {
+        const result = spawnSync(
+            'node',
+            ['./index.js', 'rgb(0, 0, 0)', 'rgb(255, 255, 255)', '-s', '-x'],
+            { encoding: 'utf-8' }
+        )
+
+        const output = result.stdout
+
+        expect(output).toContain('Accessible Color Contrast')
+        expect(output).toContain('rgb(0, 0, 0)')
+        expect(output).toContain('rgb(255, 255, 255)')
+        expect(output).toContain('Ratio: 21:1')
+        expect(output).not.toContain('Required Contrast Ratio')
+        expect(output).not.toContain('Required Font Size')
+        expect(output).not.toContain('WCAG 2.2 Conditions')
+        expect(output).toContain(
+            'Your colors meet the WCAG 2.2 AA and AAA contrast conditions.'
+        )
+    })
+
+    it('shows the expected output, although there is an additional unsupported flag', () => {
+        const result = spawnSync(
+            'node',
+            ['./index.js', 'rgb(0, 0, 0)', 'rgb(255, 255, 255)', '-x'],
+            { encoding: 'utf-8' }
+        )
+
+        const output = result.stdout
+
+        expect(output).toContain('Accessible Color Contrast')
+        expect(output).toContain('rgb(0, 0, 0)')
+        expect(output).toContain('rgb(255, 255, 255)')
+        expect(output).toContain('Ratio: 21:1')
+        expect(output).toContain('Required Contrast Ratio')
+        expect(output).toContain('Required Font Size')
+        expect(output).toContain('WCAG 2.2 Conditions')
+        expect(output).toContain(
+            'Your colors meet the WCAG 2.2 AA and AAA contrast conditions.'
+        )
+    })
+})
